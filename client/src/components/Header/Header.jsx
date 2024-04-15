e4f4b2473a12959dab720069eaa94f2258851751
VikiMaiselman <maiselmanwikki@gmail.com>
2024-04-15 14:17:53 +0300

    add sticky header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
+
+import avatar from "../../assets/images/avatar-icon.png";
 
 const navLinks = [
   {
-    path: "/home",
+    path: "/",
     display: "Home",
   },
   {
@@ -15,14 +18,29 @@ const navLinks = [
     display: "Services",
   },
   {
-    path: "/contact",
+    path: "/contacts",
     display: "Contacts",
   },
 ];
 
 export default function Header() {
+  const headerRef = React.useRef(null);
+  const menuRef = React.useRef(null);
+
+  const handleStickyHeader = () => {
+    window.addEventListener("scroll", () => {
+      if (document.body.scrollTop > 80) headerRef.current.classList.add("sticky__header");
+      else headerRef.current.classList.remove("sticky__header");
+    });
+  };
+
+  React.useEffect(() => {
+    handleStickyHeader();
+    return () => window.removeEventListener("scroll", handleStickyHeader);
+  });
+
   return (
-    <header className="header flex items-center">
+    <header className="header flex items-center" ref={headerRef}>
       <div className="container">
         <div className="flex items-center justify-between">
           {/* logo */}
@@ -39,7 +57,11 @@ export default function Header() {
                   <li>
                     <NavLink
                       to={link.path}
-                      className={(navClass) => (navClass.isActive ? "text-primaryColor text-[16px] leading-7 " : "")}
+                      className={(navClass) =>
+                        navClass.isActive
+                          ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                          : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
+                      }
                     >
                       {link.display}
                     </NavLink>
@@ -47,6 +69,27 @@ export default function Header() {
                 ))
               )}
             </ul>
+          </div>
+
+          {/* nav right */}
+          <div className="flex items-center gap-4">
+            <div className="hidden">
+              <Link to="/">
+                <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
+                  <img src={avatar} alt="" className="w-full rounded-full" />
+                </figure>
+              </Link>
+            </div>
+
+            <Link to="/login">
+              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-md">
+                Login
+              </button>
+            </Link>
+
+            <span className="md:hidden">
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
           </div>
         </div>
       </div>
